dd94418e9b07394c4a474191b0b08423c3e29d2f
Yilap88 <yilap@hotmail.com>
2025-12-02 22:41:36 -0500

    Include ANH production data
    updating ANH gas production data files for years 2013-2025

--- src/get_ANH_prod.py
+++ src/get_ANH_prod.py
@@ -4,6 +4,6 @@
 
 import pandas as pd
 
-url25 = 'https://www.anh.gov.co/documents/28388/Producci%C3%B3n_Fiscalizada_Gas_2025_4yLgd5M.xlsx'
-url24 = 'https://www.anh.gov.co/documents/26710/Producci%C3%B3n_Fiscalizada_Crudo_2024_TIBfkeo.xlsx'
-url23 = 'https://www.anh.gov.co/documents/23471/Producci%C3%B3n_Fiscalizada_Crudo_2023.xlsx'
+url = 'https://www.anh.gov.co/documents/28388/Producci%C3%B3n_Fiscalizada_Gas_2025_4yLgd5M.xlsx'
+
+xls = pd.ExcelFile(url)
