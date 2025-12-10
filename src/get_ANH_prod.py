3e510c17ebf1cce1ae75c620fbdc6165479c8ac9
Yilap88 <yilap@hotmail.com>
2025-12-09 20:27:38 -0500

    Update get_ANH_prod.py

--- src/get_ANH_prod.py
+++ src/get_ANH_prod.py
@@ -4,6 +4,9 @@
 
 import pandas as pd
 
-url = 'https://www.anh.gov.co/documents/28388/Producci%C3%B3n_Fiscalizada_Gas_2025_4yLgd5M.xlsx'
+url = 'https://www.anh.gov.co/documents/23123/Balance_producci%C3%B3n_de_Gas_2015.xlsx'
+
+data = pd.read_excel(url, sheet_name = 'enero-15', skiprows = 6, skipfooter = 3)
 
 xls = pd.ExcelFile(url)
+hojas = xls.sheet_names
